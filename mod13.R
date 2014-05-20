30a31e5ff9be9f78adda9a71290a065eee24c499
Bern67 <h2o.bern@gmail.com>
2014-05-20 12:42:13 -0700

    Clean up code for maps

--- mod13.R
+++ mod13.R
@@ -4,42 +4,45 @@
 # package of maps
 library(maps)
 library(mapdata)
+library(ggplot)
+library(gridExtra)
 
-seAK <- map("worldHires", "canada",
+# AK state map
+akmap <- map("worldHires", "canada",
             xlim=c(-180,-130), ylim=c(50, 71),
             col="gray90", fill=TRUE)
-
-seAK <- map("worldHires", "usa",
+akmap <- map("worldHires", "usa",
             xlim=c(-180,-130), ylim=c(50, 71),
             col="tan", fill=TRUE, add=TRUE)
-
-map.scale(-178.9,70.7, ratio=FALSE, relwidth=0.25, cex=1.0)
-seAK_lat <- 55.541175
-seAK_lon <- -132.855405
-points(seAK_lon, seAK_lat, col = "yellow",bg="red", pch = 21, cex = 1.2)
+map.scale(-178.9,70.7, ratio=FALSE, relwidth=0.25, cex=.5)
+akmap_lat <- 55.541175
+akmap_lon <- -132.855405
+points(akmap_lon, akmap_lat, col = "yellow",bg="red", pch = 21, cex = 1.2)
 map.axes()
 box()
 
-
-# Creates a map of Southeast AK for project report
-library(maps)
-library(mapdata)
-
-seAK <- map("worldHires", "canada",
+# Map of Southeast AK
+seakmap <- map("worldHires", "canada",
     xlim=c(-141,-129), ylim=c(54, 60),
     col="gray90", fill=TRUE)
-
-seAK <- map("worldHires", "usa",
+seakmap <- map("worldHires", "usa",
     xlim=c(-141,-129), ylim=c(54, 60),
     col="tan", fill=TRUE, add=TRUE)
-
-map.scale(-140.5,60, ratio=FALSE, relwidth=0.3, cex=1.1)
+map.scale(-140.5,60, ratio=FALSE, relwidth=0.3, cex=0.75)
+seakmap_lat <- 55.541175
+seakmap_lon <- -132.855405
+points(seakmap_lon, seakmap_lat, col = "yellow",bg="red", pch = 21, cex = 1.2)
 map.axes()
-
-seAK_lat <- 55.541175
-seAK_lon <- -132.855405
-points(seAK_lon, seAK_lat, col = "yellow",bg="red", pch = 21, cex = 1.3)
 box()
+
+# Create grid for inlay maps
+grid.newpage()
+vp_seak <- viewport(width = 1, height = 1, x = 0.5, y = 0.5) # the larger map
+vp_ak <- viewport(width = 0.3, height = 0.3, x = 0.6, y = 0.4) # the inset
+
+# plot the maps to each view point
+print(seakmap, vp = vp_seak)
+print(akmap, vp = vp_ak)
 
 
 # Close up map of POW SEAK
@@ -60,6 +63,10 @@ points(bbl_lon, bbl_lat, col = "yellow",bg="red", pch = 21, cex = 1.7)
 box()
 
 
+# Google topo of Black Bear Lake
 library(ggmap)
 bbl <- get_map("Black Bear Lake, Prince of Wales-Outer Ketchikan, AK",zoom = 14)
 ggmap(bbl)
+
+
+
